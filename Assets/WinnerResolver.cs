bcd868be37ebfd125c080a2f7a3571403e26d2e3
StaNov <stanovec@gmail.com>
2015-11-07 23:01:27 +0100

    winning condition

---
+++ Assets/WinnerResolver.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinnerResolver : MonoBehaviour {
+
+    public Transform player;
+    public Transform target;
+    public GameObject winningText;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (player.position == target.position)
+        {
+            winningText.SetActive(true);
+        }
+	}
+}
